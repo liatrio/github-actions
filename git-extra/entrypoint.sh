8edd0368f68a7dfcebae3e1907610df8f15f4af5
Casey Lee <casey@liatrio.com>
2020-03-13 12:35:46 -0700

    testing

--- git-extra/entrypoint.sh
+++ git-extra/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 set -e
 
-git fetch origin +refs/tags/*:refs/tags/*
+git fetch --unshallow origin +refs/tags/*:refs/tags/*
 VERSION=$(git describe --tags --dirty)
 echo "::set-output name=version::${VERSION}"
