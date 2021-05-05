a377c403ad0e9d26a2089a93337d81ef070793fb
Chris Schreiber <chriss@liatrio.com>
2021-05-05 16:50:18 -0700

    feat: add gitops-semver-increment-yaml action

---
+++ gitops-semver-increment-yaml/README.md
@@ -0,0 +1,14 @@
+# GitOps Semantic Version Increment YAML
+
+Increment a semantic version in a YAML file
+
+## Inputs
+- **file:** File to update version in
+- **path:** Expression path of version in YAML
+- **position:** SemVer position to increment (MAJOR|MINOR|PATCH default:PATCH)
+
+## Example
+
+```yaml
+
+```
---
+++ gitops-semver-increment-yaml/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+set -e
+
+# get old version
+PREVIOUS_VERSION=$(yq r $MANIFEST_FILE $MANIFEST_PATH)
+
+# increment version
+NEXT_VERSION=$(node /semver-increment $PREVIOUS_VERSION $SEMVER_POSITION)
+
+echo "Change version in $MANIFEST_FILE:$MANIFEST_PATH from $PREVIOUS_VERSION to $NEXT_VERSION"
+
+# Change manifest value
+#yq w -i $FILE $PATH $NEXT_VERSION
