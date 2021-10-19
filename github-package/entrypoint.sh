279faaa0a9020e825869a28148593356360cefd2
Connor Mulligan <misterc500@gmail.com>
2021-10-19 08:45:42 -0700

    fix: add sanity check for release_id and upload_url

--- github-package/entrypoint.sh
+++ github-package/entrypoint.sh
@@ -5,16 +5,18 @@ FILENAME=$(basename "${FILE}")
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 RELEASE_ID=$(jq --raw-output '.release.id' "$GITHUB_EVENT_PATH")
+echo "RELEASE_ID: ${RELEASE_ID}"
 
 tmp=${mktemp}
 
 UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
+echo "UPLOAD_URL: ${UPLOAD_URL}"
 curl \
         -sSL \
         -X POST \
-        -H ${AUTH_HEADER} \
-        --upload-file ${FILE} \
+        -H "${AUTH_HEADER}" \
+        --upload-file "${FILE}" \
         --header "Content-Type:application/octet-stream" \
         --write-out "%{http_code}" \
         --output $tmp \
-        ${UPLOAD_URL}
+        "${UPLOAD_URL}"
