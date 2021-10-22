438c4518ec1de13ff76e0912af4197255158180d
Alex Ashley <alexashley@users.noreply.github.com>
2021-10-22 10:38:07 -0400

    [gitops-gh-pr] Allow for overriding commit prefix, Git author, & bubble up failures (#21)

--- gitops-gh-pr/entrypoint.sh
+++ gitops-gh-pr/entrypoint.sh
@@ -4,20 +4,29 @@ set -e
 # Configure git
 echo "https://gitops:$GITHUB_TOKEN@github.com" > ~/.git-credentials
 git config --local credential.helper store
-git config --local user.email "gitops@liatr.io"
-git config --local user.name "GitOps Automation"
+git config --local user.email "${GIT_EMAIL}"
+git config --local user.name "${GIT_NAME}"
+
+BASE_BRANCH=$(git branch --show-current)
+BRANCH="gitops-$(git rev-parse --short HEAD)"
+
+# Check for an existing pull request
+OPEN_PRS=$(gh pr list --base "$BASE_BRANCH" --head "$BRANCH" -q 'length' --json url)
+if [ "${OPEN_PRS}" = "1" ]; then
+  echo "Skipping PR creation because a pull request already exists."
+  exit 0
+fi
 
 # Commit change
 git add .
-git commit -m "gitops: $COMMIT_MESSAGE"
+git commit -m "${COMMIT_PREFIX}: $COMMIT_MESSAGE"
 
 # Create branch
-BRANCH="gitops-$(git rev-parse --short HEAD)"
 git checkout -b $BRANCH
 git push origin $BRANCH
 
 # Create pull request
-gh pr create --fill || true
+gh pr create --fill
 
 # Merge pull request (optional)
 if [ ! -z "$MERGE_AUTO" ]; then
