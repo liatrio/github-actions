841f28a31082a61f4bcd9574dae62c47c8be7300
chodges7 <chodges7@mail.csuchico.edu>
2021-10-22 14:02:28 -0700

    chore: remane github-package to github-assets, remove second curl call

---
+++ github-assets/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+FILENAME=$(basename "${FILE}")
+echo "FILE: ${FILENAME}"
+
+# Get the latest release id, store in temporary file
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/liatrio/mobile-pipeline-poc/releases/latest > temp.json
+
+RELEASE_ID=$(jq --raw-output '.id' "temp.json")
+echo "RELEASE_ID: ${RELEASE_ID}"
+
+tmp=${mktemp}
+
+UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILE}"
+echo "UPLOAD_URL: ${UPLOAD_URL}"
+
+curl \
+    -X POST \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Content-Type:application/octet-stream" \
+    --upload-file "${FILE}" \
+    ${UPLOAD_URL}
