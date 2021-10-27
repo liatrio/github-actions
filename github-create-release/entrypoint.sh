7f1a6d355fd654073c16b7ca3d6c9556378f4c88
Connor Mulligan <misterc500@gmail.com>
2021-10-27 14:42:29 -0700

    fix: remove false as defaults, add --fail argument to curl request

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -6,6 +6,7 @@ curl \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
   -d "{\"tag_name\":\"$TAG_NAME\", \"name\":\"$NAME\", \"body\":\"$BODY\", \"draft\": \"$DRAFT\", \"prerelease\":\"$PRERELEASE\", 
-        \"discussion_category_name\":\"$DISCUSSION_CATEGORY_NAME\", \"general_release_notes\": \"$GENERAL_RELEASE_NOTES\"}" 
+        \"discussion_category_name\":\"$DISCUSSION_CATEGORY_NAME\", \"general_release_notes\": \"$GENERAL_RELEASE_NOTES\"}" \
+  --fail
 
   
