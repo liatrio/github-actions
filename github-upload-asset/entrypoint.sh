0290b581ec6cb09ca9137a07c5a8610fabc6ad68
Connor Mulligan <misterc500@gmail.com>
2021-10-28 15:00:15 -0700

    fix: syntax error, OWNER -> ORG_OWNER

--- github-upload-asset/entrypoint.sh
+++ github-upload-asset/entrypoint.sh
@@ -2,12 +2,10 @@
 
 FILENAME=$(basename "${FILE}")
 echo "FILE: ${FILENAME}"
-echo "OWNER: ${OWNER}"
-echo "GITHUB_REPOSITORY: ${GITHUB_REPOSITORY}"
-# Get the latest release id, store in temporary file
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/liatrio/mobile-pipeline-poc/releases/latest > temp.json
 
-cat temp.json
+# Get the latest release id, store in temporary file
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${ORG_OWNER}/${GITHUB_REPOSITORY}/releases/latest > temp.json
+
 
 RELEASE_ID=$(jq --raw-output '.id' "temp.json")
 echo "RELEASE_ID: ${RELEASE_ID}"
