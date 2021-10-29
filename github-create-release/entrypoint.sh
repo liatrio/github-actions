e79a44d242cde3a418103186728a03ca74ede9f5
Connor Mulligan <misterc500@gmail.com>
2021-10-29 10:47:22 -0700

    fix: add --fail flag to curl request

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -8,6 +8,7 @@ curl \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
   -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE', "'"discussion_category_name"'":"'"$DISCUSSION_CATEGORY_NAME"'",
-        "'"general_release_notes"'":'$GENERATE_RELEASE_NOTES'}'
+        "'"general_release_notes"'":'$GENERATE_RELEASE_NOTES'}' \
+  --false 
 
   
