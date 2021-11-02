1bf6fcde89fddb0b07b9fb8485d7f8639919251b
Connor Mulligan <misterc500@gmail.com>
2021-11-02 08:35:00 -0700

    fix: general_release_notes -> generate_release_notes

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -4,7 +4,7 @@ curl \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
   -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE', "'"discussion_category_name"'":"'"$DISCUSSION_CATEGORY_NAME"'",
-        "'"general_release_notes"'":'$GENERATE_RELEASE_NOTES'}' \
+        "'"generate_release_notes"'":'$GENERATE_RELEASE_NOTES'}' \
   --fail
 
   
