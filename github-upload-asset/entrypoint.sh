aa1f041d7e44b75206255d89f8254949d02c6b5f
Connor Mulligan <54652291+MisterC500@users.noreply.github.com>
2021-11-02 07:29:37 -0800

    fix: remove false as defaults, add --fail argument to curl request (#26)
    * fix: remove false as defaults, add --fail argument to curl request
    
    * fix: add false to defaults for prerelease, draft and general_release_notes
    
    * fix: syntax error, change GENERAL_RELEASE_NOTES to GENERATE_RELEASE_NOTES
    
    * fix: remove --fail flag on curl request
    
    * fix: surround boolean variables in single quotes
    
    * fix: escape singel quotes around draft,prerelease and generate_release_notes
    
    * fix: escape json in data field with ''
    
    * fix: echo tag_name, malformed tag passed to action from pipeline
    
    * fix: update RELEASE field in github-upload-asset to be Latest
    
    * fix: remove --fail flag for curl request in github-create-release action
    
    * fix: change RELEASE_ID to RELEASE in entrypoint script
    
    * fix: revert RELEASE default to latest from Latest
    
    * fix: revert RELEASE to RELEASE_ID
    
    * fix: check contents of temp.json file
    
    * fix: hardcode latest into curl to get latest release
    
    * test: echo OWNER and GITHUB_REPOSITORY
    
    * fix: syntax error, OWNER -> ORG_OWNER
    
    * test: echo OWNER and GITHUB_REPOSITORY
    
    * refactor: change field name GITHUB_REPOSITORY -> GITHUB_REPO
    
    * fix: syntax error
    
    * fix: GITHUB_REPOSITORY is deafult env var in github actions. change to REPO
    
    * fix: syntax error, GITHB_REPO -> REPO
    
    * fix: add ORG_OWNER to UPLOAD_URL
    
    * fix: add --fail flag to curl request
    
    * fix: remove echo commands from entrypoint
    
    * fix: remove echo command to print REPO
    
    * fix: change false flag to fail
    
    * fix: update curl request to get latest id, to use RELEASE not hardcoded latest

--- github-upload-asset/entrypoint.sh
+++ github-upload-asset/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh -l
 
 FILENAME=$(basename "${FILE}")
-echo "FILE: ${FILENAME}"
 
 # Get the latest release id, store in temporary file
-curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${ORG_OWNER}/${GITHUB_REPOSITORY}/releases/${RELEASE} > temp.json
+curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${ORG_OWNER}/${REPO}/releases/${RELEASE} > temp.json
+
 
 RELEASE_ID=$(jq --raw-output '.id' "temp.json")
 echo "RELEASE_ID: ${RELEASE_ID}"
 
-tmp=${mktemp}
 
-UPLOAD_URL="https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=${FILE}"
+UPLOAD_URL="https://uploads.github.com/repos/${ORG_OWNER}/${REPO}/releases/${RELEASE_ID}/assets?name=${FILENAME}"
 echo "UPLOAD_URL: ${UPLOAD_URL}"
 
 curl \
@@ -19,4 +18,5 @@ curl \
     -H "Authorization: token $GITHUB_TOKEN" \
     -H "Content-Type:application/octet-stream" \
     --upload-file "${FILE}" \
-    ${UPLOAD_URL}
+    ${UPLOAD_URL} \
+    --fail
