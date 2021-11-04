b0f2445385f91d66d2a10b2f0c0576ec310a3acf
Connor Mulligan <misterc500@gmail.com>
2021-11-04 13:24:17 -0700

    test: remove discussion_category_name

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -3,9 +3,8 @@ curl \
   -X POST \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
-  --fail-early \
-  -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE', "'"discussion_category_name"'":"'"$DISCUSSION_CATEGORY_NAME"'",
-        "'"generate_release_notes"'":'$GENERATE_RELEASE_NOTES'}'  
- # -v 
+  -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE',
+        "'"generate_release_notes"'":'$GENERATE_RELEASE_NOTES'}' \
+  --fail
 
   
