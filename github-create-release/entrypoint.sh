c4458e341720eae499cce03ce41bad1c498e7d97
Connor Mulligan <misterc500@gmail.com>
2021-11-04 13:02:48 -0700

    fix: add \ after fail flag

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -3,7 +3,7 @@ curl \
   -X POST \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
-  --fail
+  --fail \
   -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE', "'"discussion_category_name"'":"'"$DISCUSSION_CATEGORY_NAME"'",
         "'"generate_release_notes"'":'$GENERATE_RELEASE_NOTES'}'  
  # -v 
