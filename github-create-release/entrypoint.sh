33f41589aa12208a8dc7c5eca514af0c5c2f4ad4
Connor Mulligan <54652291+MisterC500@users.noreply.github.com>
2021-11-08 08:05:16 -0800

    fix: remove discussion_category_name from entrypoint script  (#28)
    * test: add verbose and trace arguments to curl request
    
    * test: remove verbose flag
    
    * test: remove fail and verbose flags
    
    * test: move fail flag before data flag
    
    * fix: add \ after fail flag
    
    * test: add fail-early flag to curl request
    
    * test: remove discussion_category_name

--- github-create-release/entrypoint.sh
+++ github-create-release/entrypoint.sh
@@ -3,7 +3,7 @@ curl \
   -X POST \
   -H "Authorization: $TOKEN_NAME $PAT" \
   https://api.github.com/repos/$GITHUB_OWNER/$GITHUB_REPO/releases \
-  -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE', "'"discussion_category_name"'":"'"$DISCUSSION_CATEGORY_NAME"'",
+  -d '{"'"tag_name"'":"'"$TAG_NAME"'", "'"name"'":"'"$NAME"'", "'"body"'":"'"$BODY"'", "'"draft"'":'$DRAFT', "'"prerelease"'":'$PRERELEASE',
         "'"generate_release_notes"'":'$GENERATE_RELEASE_NOTES'}' \
   --fail
 
