bc48ac174a0a362b84a3b1cabaf476b8944ba4a8
chodges7 <cdhodges5@gmail.com>
2022-04-04 15:20:14 -0700

    fix: use variable for magic number address

--- apprentice-action/tests/endpoint.test.js
+++ apprentice-action/tests/endpoint.test.js
@@ -1,29 +1,30 @@
 const axios = require("axios");
 const expect = require("chai").expect;
+const dockerBridgeIP = "172.17.0.1";
 
 describe("Tests to the \"/\" endpoint", () => {
     it("should return a 200 status code", async () => {
-        const res = await axios("http://172.17.0.1:80/");
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
         expect(res.status).to.equal(200);
     });
 
     it("should return a JSON object with a Message", async () => {
-        const res = await axios("http://172.17.0.1:80/");
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
         expect(res.data).to.haveOwnProperty("message");
     });
 
     it("should return a JSON object with a Timestamp", async () => {
-        const res = await axios("http://172.17.0.1:80/");
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
         expect(res.data).to.haveOwnProperty("timestamp");
     });
 
     it("should return a Message saying \"My name is ...\"", async () => {
-        const res = await axios("http://172.17.0.1:80/");
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
         expect(res.data.message).to.contain("My name is");
     });
 
     it("should return a UNIX style timestamp (numerical values only)", async () => {
-        const res = await axios("http://172.17.0.1:80/");
+        const res = await axios(`http://${dockerBridgeIP}:80/`);
         expect(res.data.timestamp).to.be.a("number");
     });
 });
