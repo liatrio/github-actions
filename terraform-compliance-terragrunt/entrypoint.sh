adaf88d1945c4da388e87f97a62031014f4f439a
Andrew Haller <andrew.haller@liatrio.com>
2022-12-09 17:49:33 -0600

    Adding entrypoint contents.

---
+++ terraform-compliance-terragrunt/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -n "$GH_PAT" ]; then
+  git config --global url."https://${GITHUB_ACTOR}:${GH_PAT}@github.com".insteadOf ssh://git@github.com
+fi
+
+export TARGET_FOLDER="${TARGET_FOLDER:-.}"
+export SUBFOLDERS="${SUBFOLDERS:-.}"
+export SUBFOLDERS=($SUBFOLDERS)
+
+# Use $RANDOM to avoid naming collisions
+command_opts=(
+ -p "${RANDOM}_${PLAN_FILE}"
+ -f "${COMPLIANCE_FEATURES}"
+)
+for param in "${COMPLIANCE_COMMAND_OPTS[@]}"; do command_opts+=($param); done
+export COMPLIANCE_COMMAND_OPTS="${command_opts[@]}"
+export COMPLIANCE_COMMAND=terraform-compliance
+
+cd $TARGET_FOLDER
+
+printf "Current Target: %s\n" "$(pwd)"
+
+for folder in "${SUBFOLDERS[@]}"; do
+  fulldir="${TARGET_FOLDER}/${folder}"
+  echo "##################################################################################################################"
+  echo "TERRAFORM COMPLIANCE PATH ${fulldir}"
+  echo "##################################################################################################################"
+  pushd $folder
+  printf "Current Subfolder: %s\n" "$(pwd)"
+  terragrunt plan -lock=false --out plan.out
+  terragrunt show -json plan.out > "${RANDOM}_${PLAN_FILE}"
+  printf "Running '%s' ...\n" "${COMPLIANCE_COMMAND} ${COMPLIANCE_COMMAND_OPTS[@]}"
+  ${COMPLIANCE_COMMAND} ${COMPLIANCE_COMMAND_OPTS[@]}
+  echo "##################################################################################################################"
+  rm "${RANDOM}_${PLAN_FILE}"
+  popd
+done
