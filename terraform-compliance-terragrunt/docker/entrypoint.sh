1a50998d54a42b1753b58325f474e3597d752efd
Andrew Haller <andrew.haller@liatrio.com>
2022-12-12 18:10:58 -0600

    Action updates.

--- terraform-compliance-terragrunt/docker/entrypoint.sh
+++ terraform-compliance-terragrunt/docker/entrypoint.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
-if [ -n "$GH_PAT" ]; then
-  git config --global url."https://${GITHUB_ACTOR}:${GH_PAT}@github.com".insteadOf ssh://git@github.com
+if ${GITCONFIG_ENABLED:-false} 2>/dev/null ; then
+  export GITCONFIG="${HOME}/.gitconfig"
+
+  if [ ! -f "${GITCONFIG}" -a -n "${GITCONFIG_BASE64}" ]; then
+    printf "Decoding gitconfig and saving to '%s' ...\n" "${GITCONFIG}"
+    echo "${GITCONFIG_BASE64}" | base64 --decode --ignore-garbage | sed "s/${GITCONFIG_BASE64_KEY}/${GH_PAT}/g" > "${GITCONFIG}"
+  fi
+
+  if [ -f "${GITCONFIG}" ]; then
+    contents=$(echo $(cat "${GITCONFIG}"))
+    if [ -z "$contents" ]; then
+      printf "File '%s' is empty. Removing ...\n" "${GITCONFIG}"
+      rm -f "${GITCONFIG}"
+    fi
+  fi
+
+  if [ ! -f "${GITCONFIG}" -a -n "${GH_PAT}" ]; then
+    printf "Creating global gitconfig at '%s' ...\n" "${GITCONFIG}"
+    git config --global url."https://${GITHUB_ACTOR}:${GH_PAT}@github.com".insteadOf ssh://git@github.com
+  fi
 fi
 
 export TARGET_FOLDER="${TARGET_FOLDER:-.}"
@@ -11,7 +29,7 @@ export SUBFOLDERS="${SUBFOLDERS:-.}"
 export SUBFOLDERS=($SUBFOLDERS)
 
 # Use $RANDOM to avoid naming collisions
-export RANDOM_PLAN_FILE="${RANDOM}_${PLAN_FILE}"
+export RANDOM_PLAN_FILE="${PLAN_FILE}.${RANDOM}"
 export RANDOM_PLAN_FILE_JSON="${RANDOM_PLAN_FILE}.json"
 command_opts=(
  -p "${RANDOM_PLAN_FILE_JSON}"
@@ -34,7 +52,10 @@ for folder in "${SUBFOLDERS[@]}"; do
   printf "Current Subfolder: %s\n" "$(pwd)"
   ls -l
   [ -f "${PLAN_FILE}" ] || terragrunt plan -lock=false --out "${PLAN_FILE}"
-  terragrunt show -json "${PLAN_FILE}" > "${RANDOM_PLAN_FILE_JSON}"
+  printf "Renaming '%s' to '%s' ...\n" "${PLAN_FILE}" "${RANDOM_PLAN_FILE}"
+  mv "${PLAN_FILE}" "${RANDOM_PLAN_FILE}"
+  printf "Creating json planfile '%s' ...\n" "${RANDOM_PLAN_FILE_JSON}"
+  terragrunt show -json "${RANDOM_PLAN_FILE}" > "${RANDOM_PLAN_FILE_JSON}"
   printf "Running '%s' ...\n" "${COMPLIANCE_COMMAND} ${COMPLIANCE_COMMAND_OPTS[@]}"
   ${COMPLIANCE_COMMAND} ${COMPLIANCE_COMMAND_OPTS[@]}
   echo "##################################################################################################################"
